8cb26e06079e7ccf1226b27843abaa33ef8ae3a2
Innocent Asiimwe <asiimwe@outlook.com>
2017-05-28 23:24:41 +0300

    implementing the return_third_item method to handle the list data type

--- data_type/datatype.py
+++ data_type/datatype.py
@@ -8,13 +8,22 @@ def data_type(given):
     elif type(given) is int:
         return compare_to_100(given)
     elif type(given) is list:
-        return_third_item(given)
+        return return_third_item(given)
     else:
         return 'type not catered for!'
+
 def compare_to_100(value):
+    """Method handles how intergers compare to 100"""
     if value < 100:
         return 'less than 100'
     elif value == 100:
         return 'equal to 100'
     else:
         return 'more than 100'
+
+def return_third_item(alist):
+    """method returns the third item in a list or None if none exists"""
+    if len(alist) >= 3:
+        return alist[2]
+    else:
+        return None
